1fd16f10f0f595c2b917fade63523ce946f2599e
korczn <korczyyn@gmail.com>
2024-12-22 21:53:05 +0100

    Klasa obsługująca dane graczy

--- SportsTeamManagementSys/Program.cs
+++ SportsTeamManagementSys/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Tworzenie klasy Player z właściwościami
+public class Player 
+{
+    public string Name { get; set; }
+    public int Score { get; set; }
+    public string Position { get; set; }
+
+    // Konstruktor inicjalizujący obiekt Player
+    public Player(string name, int score, string position)
+    {
+        Name = name;
+        Score = score;
+        Position = position;
+    }
+
+    // Nadpisanie metody ToString(), aby zwrócić obiekt Player jako tekst
+    public override string ToString()
+    {
+        return $"Name: {Name}, Score: {Score}, Position: {Position}";
+    }
+}
